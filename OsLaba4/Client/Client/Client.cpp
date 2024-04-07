dfea230eb3d36c04ce27f98a7183d8eaba07d222
tssvett <110456135+tssvett@users.noreply.github.com>
2024-04-07 22:30:44 +0300

    Add files via upload

---
+++ OsLaba4/Client/Client/Client.cpp
@@ -0,0 +1,59 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include<WinSock2.h>
+#include<iostream>
+#include<Windows.h>
+#include<math.h>
+#pragma warning(disable:4996)
+
+
+
+void getWords(SOCKET Connection)
+{
+	char buffer[1024] = { 0 };
+	std::cout << "Waiting for words...\n" << std::endl;
+
+	while (recv(Connection, (char*)&buffer, 1024, NULL) > 0) {
+		std::cout << "[RECEIVE]: " << "[" << buffer << "]" << std::endl;
+		memset(buffer, 0, sizeof(buffer));
+	}
+}
+
+
+void initWSA()
+{
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0)
+	{
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+}
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "Russian");
+	initWSA();
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN);
+
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	// проверяем на подключение к серверу
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr))) {
+		std::cout << "Failure in connection to server!" << '\n';
+		return 1;
+	}
+	else {
+		std::cout << "Connected!" << std::endl;
+		getWords(Connection);
+		closesocket(Connection);
+		closesocket(sListen);
+		WSACleanup();
+	}
+}
---
+++ OsLaba4/Server/Server/Server.cpp
@@ -0,0 +1,147 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <string>
+
+#pragma warning(disable: 4996)
+
+SOCKET Connections[100];
+int connectionsCounter = 0;
+const char* CD_COMMAND = "cd D:\\OneDrive\\Документы\\GitHub\\OsLaba4\\Client\\x64\\Debug";
+const char* START_COMMAND = "start D:\\OneDrive\\Документы\\GitHub\\OsLaba4\\Client\\x64\\Debug\\Client.exe";
+
+
+
+
+void initLibrary()
+{
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0)
+	{
+		std::cout << "Error loading WSA" << std::endl;
+		exit(1);
+	}
+}
+
+
+void startClientFromTerminal()
+{
+
+	// проверка на случай непредвиденных обстоятельств
+	int command_not_execute = system(CD_COMMAND);
+	if (command_not_execute)
+	{
+		std::cout << "Failed to start command" << '\n';
+		exit(1);
+	}
+	system(START_COMMAND);
+}
+
+
+void giveWordsToClient(SOCKET newConnection, int amount_of_clients, int i, std::string filename) {
+	std::ifstream file(filename);
+	std::string word;
+	int count = 0;
+
+	while (file >> word) {
+		if (count % amount_of_clients == i) {
+			std::cout << "[SEND] " <<"[" << word <<"]" << " [ TO " << newConnection << "]" << std::endl;
+			int result = send(newConnection, word.c_str(), word.length(), NULL);
+			if (result == SOCKET_ERROR)
+			{
+				std::cout << "Error with sending word: " << word << std::endl;
+				std::cout <<"Error code: " << WSAGetLastError() << std::endl;
+				exit(1);
+			}
+		}
+		count++;
+	}
+	file.close();
+}
+
+void createAndConnect(SOCKET sListen, SOCKADDR_IN addr, int sizeofaddr, int amount_of_clients, std::string filename)
+{
+	SOCKET newConnection;
+	for (int i = 0; i < amount_of_clients; ++i)
+	{
+		startClientFromTerminal();
+		newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+		if (newConnection == 0)
+		{
+			std::cout << "Socket Connection Error" << std::endl;
+		}
+		else
+		{
+			std::cout << "Client connected Successfully!" << std::endl;
+			giveWordsToClient(newConnection, amount_of_clients, i, filename);
+		}
+		Connections[i] = newConnection;
+		++connectionsCounter;
+	}
+}
+
+
+
+int getSocketNumber()
+{
+	int clientNumber;
+	int leftBorder = 3;
+	int rightBorder = 10;
+	std::cout << "Input number of Clients (n): ";
+	while (!(std::cin >> clientNumber) || (std::cin.peek() != '\n') || (clientNumber < leftBorder) || (clientNumber > rightBorder)) {
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Error :c Input correct number of Clients..." << '\n';
+	}
+	return clientNumber;
+}
+
+
+
+bool isCorrectFileName(std::string filename) {
+
+	std::ifstream file(filename);
+	if (!file) {
+		file.close();
+		return false;
+	}
+	return true;
+}
+
+
+std::string getFileName() {
+	std::string filename;
+	std::cout << "Input filename: ";
+	while (!(std::cin >> filename) || (std::cin.peek() != '\n') || !isCorrectFileName(filename)) {
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Error :c Input correct filename..." << '\n';
+	}
+	return filename;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	initLibrary();
+	int clientNumber = getSocketNumber();
+	std::string filename = getFileName();
+
+	SOCKADDR_IN addr;
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN);
+
+	createAndConnect(sListen, addr, sizeof(addr), clientNumber, filename);
+	system("pause");
+	return 0;
+}
