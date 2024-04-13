cc984b4e71f9390f8971cd20319cd502f41c091e
tssvett <110456135+tssvett@users.noreply.github.com>
2024-04-13 18:17:26 +0300

    init commit

---
+++ OsLaba5/Client/Client/Client.cpp
@@ -0,0 +1,224 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <random>
+#pragma warning(disable:4996)
+
+int inputCorrectInteger(std::string text)
+
+{
+    int polynom_degree;
+    std::cout << text << std::endl;
+    while (!(std::cin >> polynom_degree) || (std::cin.peek() != '\n')) {
+        std::cin.clear();
+        while (std::cin.get() != '\n');
+        std::cout << "Ошибка :c Введите корректное целое число..." << '\n';
+    }
+    return polynom_degree;
+}
+
+int inputCorrectBase(std::string text)
+{
+    int polynom_degree;
+    std::cout << text << std::endl;
+    while (!(std::cin >> polynom_degree) || (std::cin.peek() != '\n') || ((polynom_degree !=2) && (polynom_degree != 16))) {
+        std::cin.clear();
+        while (std::cin.get() != '\n');
+        std::cout << "Ошибка :c Введите корректное целое число..." << '\n';
+    }
+    return polynom_degree;
+}
+
+bool continueCheck()
+/*
+Функция ждет ввод пользователя.
+Если пользователь вводит Y или y, функция возвращает 1.
+Если пользователь вводит N или n, функция возвращает 0.
+Если пользователь вводит не символы выше, проверка зацикливается и будет ждать символы выше.
+*/
+{
+    bool flag = true;
+    char symbol;
+    std::cout << "Хотите продолжить(Y/y) или преостановить(N/n) программу?" << '\n';
+    while (!(std::cin >> symbol) || flag)
+    {
+        std::cin.clear();
+        if ((symbol == 'N' || symbol == 'n') && (std::cin.peek() == '\n'))
+        {
+            return false;
+        }
+        else if ((symbol == 'Y' || symbol == 'y') && (std::cin.peek() == '\n'))
+        {
+            return true;
+        }
+        while (std::cin.get() != '\n');
+        std::cout << "Ошибка :c Для работы доступны только продолжение (Y/y) и прекращение (N/n) работы.";
+    }
+}
+
+
+HANDLE hSemaphore;
+HANDLE pool[2] = {
+    OpenEvent(EVENT_ALL_ACCESS, FALSE, L"availible"),
+    OpenEvent(EVENT_ALL_ACCESS, FALSE, L"exit")
+};
+
+DWORD WINAPI ClientThread(LPVOID lpParam)
+{
+    // Получение сокета из параметра
+    SOCKET clientSocket = (SOCKET)lpParam;
+    bool is_iterated = true;
+    while (is_iterated)
+    {
+        std::cout << "Дождался! Начинем общение с сервером" << std::endl;
+
+        // Ввод числа и системы счисления
+
+        int num = inputCorrectInteger("Введите число для перевода: ");
+        int base = inputCorrectBase("Введите систему счисления (2, 16)");
+
+        // Отправка числа и системы счисления на сервер
+        std::cout << "Отправляю число " << num << " и систему счисления " << base << " на сервер" << std::endl;
+        if (send(clientSocket, (char*)&num, sizeof(num), 0) == SOCKET_ERROR)
+        {
+            std::cout << "Ошибка при отправке данных" << std::endl;
+            break;
+        }
+        if (send(clientSocket, (char*)&base, sizeof(base), 0) == SOCKET_ERROR)
+        {
+            std::cout << "Ошибка при отправке данных" << std::endl;
+            break;
+        }
+
+        // Получение результата от сервера
+        char serverResponse [1024];
+        memset(serverResponse, 0, 1024);
+        if (recv(clientSocket, serverResponse, sizeof(serverResponse), 0) == SOCKET_ERROR)
+        {
+            std::cout << "Ошибка при получении данных" << std::endl;
+            break;
+        }
+
+        // Вывод результата
+        std::cout << "Получаю ответ сервера: " << serverResponse << std::endl;
+
+        memset(serverResponse, 0, 1024);
+
+        bool continueFlag = continueCheck();
+        if (send(clientSocket, (char*)&continueFlag, sizeof(continueFlag), 0) == SOCKET_ERROR)
+        {
+            std::cout << "Ошибка при отправке данных" << std::endl;
+            break;
+        }
+        if (continueFlag)
+        {
+            std::cout << "Продолжаю работу" << std::endl;
+        }
+        else {
+            std::cout << "Приостанавливаю свою работу" << std::endl;
+            DWORD dwWaitResult = WaitForSingleObject(pool[1], INFINITE);
+            switch (dwWaitResult) {
+            case WAIT_OBJECT_0:
+                std::cout << "Сигнал для снятия с приостановки обнаружен!" << std::endl;
+                break;
+            case WAIT_ABANDONED:
+                std::cout << "Adandoned." << std::endl;
+                break;
+            case WAIT_TIMEOUT:
+                std::cout << "TIMEOUT." << std::endl;
+                break;
+            case WAIT_FAILED:
+                std::cout << "FAIL." << std::endl;
+                std::cout << GetLastError() << std::endl;
+                break;
+            default:
+                std::cout << "Чето случилось.." << GetLastError() << std::endl;
+            }
+            std::cout << "Приостановка работы завершена" << std::endl;
+        }
+    }
+
+    return 0;
+}
+
+void initWSA()
+{
+    WSAData wsaData;
+    WORD DLLVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DLLVersion, &wsaData) != 0)
+    {
+        std::cout << "Error" << std::endl;
+        exit(1);
+    }
+}
+
+SOCKET createClientSocket() {
+    // Создание сокета
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        std::cout << "Ошибка при создании сокета" << std::endl;
+        WSACleanup();
+        return -1;
+    }
+    return clientSocket;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    initWSA();
+    SOCKET clientSocket = createClientSocket();
+
+    // Подключение к серверу
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_port = htons(5555);
+    if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        std::cout << "Ошибка при подключении к серверу" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    //Открытие семафора
+    hSemaphore = OpenSemaphore(SYNCHRONIZE, FALSE, L"semaphore");
+    if (hSemaphore == NULL) {
+        std::cerr << "Ошибка при открытии семафора" << std::endl;
+    }
+
+    if (pool[0] == NULL) {
+        std::cerr << "Ошибка при открытии флага 0" << std::endl;
+    }
+    if (pool[1] == NULL) {
+        std::cerr << "Ошибка при открытии флага 1" << std::endl;
+    }
+   
+    // Ожидание события
+    std::cout << "Нахожусь в режиме ожидания..." << std::endl;
+
+    DWORD dwWaitResult = WaitForSingleObject(hSemaphore, INFINITE);
+    // Создание потока клиента
+    HANDLE hThread = CreateThread(NULL, 0, ClientThread, (LPVOID)clientSocket, 0, NULL);
+    if (hThread == NULL)
+    {
+        std::cout << "Ошибка при создании потока" << std::endl;
+        CloseHandle(pool);
+        closesocket(clientSocket);
+        WSACleanup();
+        return -1;
+    }
+    // Ожидание завершения потока
+    WaitForSingleObject(hThread, INFINITE);
+
+    // Освобождение ресурсов
+    CloseHandle(hThread);
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ OsLaba5/Server/Server/Server.cpp
@@ -0,0 +1,247 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <WinSock2.h>
+#include <windows.h>
+#include <string.h>
+#include <vector>
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+
+
+std::vector<SOCKET> Sockets; //вектор для сокетов
+std::vector<bool> Sockets_is_sleep;
+int n;
+HANDLE hSemaphore;
+HANDLE pool[2] = {
+        CreateEvent(NULL, FALSE, FALSE, L"availible"),
+        CreateEvent(NULL, FALSE, FALSE, L"exit")
+};
+
+std::string toHex(int num)
+{
+    std::string hex;
+    const char hex_digits[] = "0123456789ABCDEF";  // Массив шестнадцатеричных цифр
+
+    while (num > 0)
+    {
+        hex.insert(0, 1, hex_digits[num % 16]);  // Добавляем шестнадцатеричную цифру в начало строки в зависимости от остатка от деления на 16
+        num /= 16;                                // Делим число на 16
+    }
+
+    return hex;
+}
+
+std::string toBin(int num)
+{
+    std::string binary;
+
+    while (num > 0)
+    {
+        binary.insert(0, 1, (num % 2) + '0');  // Добавляем '0' или '1' в начало строки в зависимости от остатка от деления на 2
+        num /= 2;                               // Делим число на 2
+    }
+
+    return binary;
+}
+
+std::string getResult(int num, int base) {
+	std::string result;
+    switch (base)
+    {
+    case 2:
+        result = toBin(num);
+        break;
+    case 16:
+        result = toHex(num);
+        break;
+    default:
+        result = "Неизвестная система счисления";
+    }
+    return result;
+}
+
+DWORD WINAPI ServerThread(int current_socket)
+{
+    WaitForSingleObject(hSemaphore, INFINITY);
+    bool continueFlag = true;
+    SOCKET lastConnectedSocket = Sockets[current_socket];
+    while (continueFlag) {
+        // Получение числа и системы счисления от клиента
+        int num;
+        int base;
+
+        if (recv(lastConnectedSocket, (char*)&num, sizeof(num), 0) == SOCKET_ERROR)
+        {
+            std::cout << "Ошибка при получении данных" << std::endl;
+            exit(1);
+        }
+        if (recv(lastConnectedSocket, (char*)&base, sizeof(base), 0) == SOCKET_ERROR)
+        {
+            std::cout << "Ошибка при получении данных" << std::endl;
+            exit(1);
+        }
+
+        // Преобразование числа в другую систему счисления
+        std::cout << "Преобразуем " << num << " в " << base <<"ичную систему счисления" << std::endl;
+        std::string result = getResult(num, base);
+        std::cout << "Результат: " << result << std::endl;
+
+        if (send(lastConnectedSocket, result.c_str(), result.length(), 0) == SOCKET_ERROR)
+        {
+            std::cout << "Ошибка при отправке данных" << std::endl;
+            exit(1);
+
+        }
+        if (recv(lastConnectedSocket, (char*)&continueFlag, sizeof(continueFlag), 0) == SOCKET_ERROR)
+        {
+            std::cout << "Ошибка при получении данных" << std::endl;
+            exit(1);
+        }
+    }
+
+    std::cout << "Клиент " << lastConnectedSocket << " приостановил свою работу" << std::endl;
+    Sockets_is_sleep[current_socket] = 1;
+    std::cout << "Отправляю сигнал семафору о освобождении" << std::endl;
+    ReleaseSemaphore(hSemaphore, 1, NULL);
+}
+
+void initWSA()
+{
+    WSAData wsaData;
+    WORD DLLVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DLLVersion, &wsaData) != 0)
+    {
+        std::cout << "Error" << std::endl;
+        exit(1);
+    }
+}
+
+void start_client()
+{
+    const char* navigation_command = "cd D:\\OneDrive\\Документы\\GitHub\\OsLaba5\\Client\\x64\\Debug";
+    const char* run_command = "start D:\\OneDrive\\Документы\\GitHub\\OsLaba5\\Client\\x64\\Debug\\Client.exe";
+    // проверка на случай непредвиденных обстоятельств
+    int navigation_failure = system(navigation_command);
+    if (navigation_failure)
+    {
+        std::cout << "Failure in system navigation!" << '\n';
+        exit(1);
+    }
+    // запускаем собранную программу у клиента
+    system(run_command);
+}
+
+int inputCorrectInteger(std::string text)
+
+{
+    int polynom_degree;
+    std::cout << text << std::endl;
+	int leftBorder = 1;
+    int rightBorder = 5;
+    while (!(std::cin >> polynom_degree) || (std::cin.peek() != '\n') || (polynom_degree < leftBorder && polynom_degree > rightBorder)) {
+        std::cin.clear();
+        while (std::cin.get() != '\n');
+        std::cout << "Ошибка :c Введите корректное целое число..." << '\n';
+    }
+    return polynom_degree;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+    // Инициализация Winsock
+	initWSA();
+    n = inputCorrectInteger("Введите количество клиентов для запуска: ");
+
+    // Создание сокета
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET)
+    {
+        std::cout << "Ошибка при создании сокета" << std::endl;
+        WSACleanup();
+        return -1;
+    }
+    std::cout << "Сокет сервера запущен успешно!" << std::endl;
+
+    // Настройка сокета
+    sockaddr_in serverAddr;
+	int serverSize = sizeof(serverAddr);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(5555);
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        std::cout << "Ошибка при настройке сокета" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+	// Ожидание прослушивания с максимальной длинной очереди
+	std::cout << "Ожидание подключения клиента..." << std::endl;
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
+    {
+        std::cout << "Ошибка при ожидании подключения" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+
+
+    // Создание семафора и события
+    hSemaphore = CreateSemaphore(NULL, n, n, L"semaphore");
+
+    for (int i = 0; i < n; i++) {
+        start_client();
+    }
+
+
+    int current_socket = 0;
+    SOCKET lastConnectedSocket;
+    while (true) {
+        if (Sockets.size() < n) {
+            Sockets.push_back(accept(serverSocket, (SOCKADDR*)&serverAddr, &serverSize));
+            Sockets_is_sleep.push_back(0);
+            if (Sockets[Sockets.size() - 1] == INVALID_SOCKET)
+            {
+                std::cout << "Ошибка при принятии подключения" << std::endl;
+                closesocket(serverSocket);
+                WSACleanup();
+                return -1;
+            }
+            std::cout << "Клиент " << Sockets[Sockets.size() - 1] << " подключился успешно!" << std::endl;
+        }
+        if (Sockets_is_sleep[current_socket] == 1) {
+            std::cout << "Снимаю приостановку с клиента " << Sockets[current_socket] << std::endl;
+            SetEvent(pool[1]);
+        }
+        std::cout << "Номер сокета, с которым мы будем работать: " << Sockets[current_socket] << std::endl;
+
+        lastConnectedSocket = Sockets[current_socket];
+     
+        // Создание потока сервера
+        HANDLE hThread = CreateThread(NULL,NULL, (LPTHREAD_START_ROUTINE)ServerThread, (LPVOID)current_socket, NULL, NULL);
+        if (hThread == NULL)
+        {
+            std::cout << "Ошибка при создании потока" << std::endl;
+            closesocket(lastConnectedSocket);
+        }
+        DWORD dwWaitResult =  WaitForSingleObject(hThread, INFINITE);
+
+        CloseHandle(hThread);
+        current_socket++;
+        if (current_socket == n) {
+            current_socket = 0;
+        }
+        std::cout << "==================================================================================" << std::endl;
+
+    }
+    // Освобождение ресурсов
+    CloseHandle(hSemaphore);
+    CloseHandle(pool);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
